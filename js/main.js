132c9e1dd769fd97c4172a9235089c7249d38569
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2021-10-19 20:46:52 -0300

    Inclusão no HTML,criação de  classes no CSS e  de funções no Javascript.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
-function mensage() {
-    console.log('olá');
-}
+$(".user-action").click(function() {
+    $(".subtext").hide();
+    $(".thick-line").hide();
+    $(".img-cash").hide();
+    $(".user-action").hide();
+    $("#steps-page").show();
+})
 
-//
+
+
+$("#steps-page").steps({
+    headerTag: "h3",
+    bodyTag: ".content",
+    transitionEffect: "slideLeft",
+    autoFocus: true
+});
