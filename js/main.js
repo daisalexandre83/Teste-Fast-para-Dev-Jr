11c36085eeea59952d168b2cd5c0e19d369b8c7f
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2021-11-04 23:21:43 -0300

    Inclusão no HTML,nas classes do CSS, na função do JS  e imagens.

--- js/main.js
+++ js/main.js
@@ -5,8 +5,8 @@ $(".user-action").click(function() {
     $(".user-action").hide();
     $(".steps-page").show();
     $(".wizard > .actions a").hide();
-    $("#steps-page-content-one-part2").hide();
-    $("#steps-page-content-one-part3").hide();
+    $("#steps-page1-radio2").hide();
+    $("#steps-page1-form-checkbox").hide();
     $(".page-subtext").hide();
 })
 
@@ -19,19 +19,19 @@ $(".steps-page").steps({
 });
 
 $("#yes-option-part1").click(function () {
-    $("#steps-page-content-one-part2").show();
+    $("#steps-page1-radio2").show();
 })
 
 $("#yes-option-part2").click(function () {
-    $("#steps-page-content-one-part3").show();
+    $("#steps-page1-form-checkbox").show();
 })
 
 
- $(".steps-page-ckeckbox").click(function () {
+ $(".steps-page1-ckeckbox").click(function () {
 
-    let checkbox1 = $('#input1-checkbox');
-    let checkbox2 = $('#input2-checkbox');
-    let checkbox3 = $('#input3-checkbox');
+    let checkbox1 = $('#steps-page1-ckeckbox1');
+    let checkbox2 = $('#steps-page1-ckeckbox2');
+    let checkbox3 = $('#steps-page1-ckeckbox3');
 
     if(checkbox1.is(":checked") && checkbox2.is(":checked") && checkbox3.is(":checked")){
         $(".wizard > .actions a").show();
