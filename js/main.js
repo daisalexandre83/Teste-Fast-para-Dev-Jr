d3f934b5936e0240fc059add0a5f0697925ddc17
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2021-11-20 22:58:06 -0300

    Alterações no HTML,nas classes do CSS e nas funcões do Javascript

--- js/main.js
+++ js/main.js
@@ -3,24 +3,22 @@ $("#user-action").click(function() {
     $(".page-thick-line").hide();
     $(".img-cash").hide();
     $("#user-action").hide();
-    $(".steps-page").show();
-    $(".wizard > .actions a").hide();
-    $("#steps-page1-radio2").hide();
-    $("#steps-page1-form-checkbox").hide();
-    $(".page-subtext").hide();
+     $("#steps").show();
+    $("#step-1-radio2").hide();
+    $(".page-subtext").hide(); 
 })
 
-  /* jQuery(document).ready(function () {
+jQuery(document).ready(function () {
+   
     var navListItems = $('ul.setup-panel li a');
         allWells = $('.setup-content');
-
-        allWells.hide();
-
+    
+    allWells.hide();
     navListItems.click(function(e) {
         e.preventDefault();
         var $target = $($(this).attr('href')),
-        $item = $(this).closest('li'),
-        
+        $item = $(this).closest('li');
+
         if (!$item.hasClass('disabled')) {
             navListItems.closest('li').removeClass('active');
             $item.addClass('active');
@@ -28,88 +26,60 @@ $("#user-action").click(function() {
             $target.show();
         }
     });
-
     $('ul.setup-panel li.active a').trigger('click');
 
-     $('#activate-step-2').on('click',function(e) {
-        $('ul.setup-panel li:eq(1)').removeClass('disabled');
-        $('ul.setup-panel li a[href="#step-2"]').trigger('click');
-        $(this).remove();
+    $('#activate-step-2').on('click',function(e) {
+    // $('ul.setup-panel li:eq(1)').removeClass('disabled');
+    $('ul.setup-panel li a[href="#step-2"]').trigger('click');
+    $(this).remove();
     })
 
     $('#activate-step-3').on('click',function(e) {
-        $('ul.setup-panel li:eq(2)').removeClass('disabled');
-        $('ul.setup-panel li a[href="#step-3"]').trigger('click');
-        $(this).remove();
+    // $('ul.setup-panel li:eq(2)').removeClass('disabled');
+    $('ul.setup-panel li a[href="#step-3"]').trigger('click');
+    $(this).remove();
     }) 
+
+    $('#activate-step-4').on('click',function(e) {
+        // $('ul.setup-panel li:eq(3)').removeClass('disabled');
+        $('ul.setup-panel li a[href="#step-4"]').trigger('click');
+        $(this).remove();
+        }) 
+
+    $('#activate-step-5').on('click',function(e) {
+        // $('ul.setup-panel li:eq(4)').removeClass('disabled');
+        $('ul.setup-panel li a[href="#step-5"]').trigger('click');
+        $(this).remove();
+        })     
+
 }); 
- 
- */
-
-jQuery(document).ready(function() {
-    
-    var navListItems = $('ul.setup-panel li a'),
-        allWells = $('.setup-content');
-
-    allWells.hide();
-
-    navListItems.click(function(e)
-    {
-        e.preventDefault();
-        var $target = $($(this).attr('href')),
-            $item = $(this).closest('li');
-        
-        if (!$item.hasClass('disabled')) {
-            navListItems.closest('li').removeClass('active');
-            $item.addClass('active');
-            allWells.hide();
-            $target.show();
-        }
-    });
-    
-    $('ul.setup-panel li.active a').trigger('click');
-    
-    // DEMO ONLY //
-    $('#activate-step-2').on('click', function(e) {
-        $('ul.setup-panel li:eq(1)').removeClass('disabled');
-        $('ul.setup-panel li a[href="#step-2"]').trigger('click');
-        $(this).remove();
-    })   
-    
-    $('#activate-step-3').on('click', function(e) {
-        $('ul.setup-panel li:eq(2)').removeClass('disabled');
-        $('ul.setup-panel li a[href="#step-3"]').trigger('click');
-        $(this).remove();
-    })    
-});
 
 
-
-
-
-
-
-
-$("#yes-option-part1").click(function () {
-    $("#steps-page1-radio2").show();
+$("#step-1-yes1-option").click(function () {
+    $("#step-1-radio2").show();
 })
 
-$("#yes-option-part2").click(function () {
-    $("#steps-page1-form-checkbox").show();
+$("#step-1-yes2-option").click(function () {
+    $(".step-1-form-checkbox").show();
+   
 })
 
 
-$(".steps-page1-ckeckbox").click(function () {
+$(".step-1-ckeckbox").click(function () {
 
-let checkbox1 = $('#steps-page1-ckeckbox1');
-let checkbox2 = $('#steps-page1-ckeckbox2');
-let checkbox3 = $('#steps-page1-ckeckbox3');
+let checkbox1 = $('#step-1-confirm1');
+let checkbox2 = $('#step-1-confirm2');
+let checkbox3 = $('#step-1-confirm3');
 
 if(checkbox1.is(":checked") && checkbox2.is(":checked") && checkbox3.is(":checked")){
-    $(".wizard > .actions a").show();
-}
+    $("#activate-step-2").show();
+ }
 
 }) 
+
+/* $("#activate-step-2").click(function name(params) {
+    
+}) */
 
  $(".steps-page2-bank").click(function() {
   console.log('daiane');
